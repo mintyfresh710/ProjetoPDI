d1e211a701bfe5114d69e5a4c0b67a98b2686760
Konkvon <joaopedroorem@gmail.com>
2025-05-16 01:54:05 -0300

    Atualização sistema cadastro/login e merge com a main branch
    Atualização sistema cadastro/login adicionando banco de dados que requisita do forms e merge com a main branch

---
+++ projeto_pdi/app_pdi/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+from .models import Usuarios
+
+def show_login(request):
+    return render(request, 'index.html', {'page': 'login'})
+
+def show_cadastro(request):
+    return render(request, 'index.html', {'page': 'cadastro'})
+
+def cadastro_view(request):
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        confirmar = request.POST.get("confirmar_senha")
+
+        if senha == confirmar:
+            usuario = Usuarios(
+                nome=nome,
+                email=email,
+                senha=make_password(senha)
+            )
+            usuario.save()
+            return redirect('login')
+        else:
+            return render(request, "index.html", {"erro": "Senhas não coincidem"})
+
+    return render(request, "index.html", {"page": "cadastro"})
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+
+        try:
+            usuario = Usuarios.objects.get(email=email)
+            if check_password(senha, usuario.senha):
+                # Login bem-sucedido
+                return redirect('home')
+            else:
+                erro = "Senha incorreta"
+        except Usuarios.DoesNotExist:
+            erro = "Usuário não encontrado"
+
+        return render(request, "index.html", {"erro": erro})
+
+    return render(request, "index.html")
+
