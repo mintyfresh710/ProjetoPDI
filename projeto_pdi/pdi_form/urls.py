2c8b73e2aca91fb1eac704c55bc9061384e70295
jasmim <guilhermejasmim@gmail.com>
2025-06-11 10:24:34 -0300

    Add files via upload

--- projeto_pdi/pdi_form/urls.py
+++ projeto_pdi/pdi_form/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('salvar/', views.salvar_pdi, name='salvar_pdi'),   
     path('visualizar/<int:pdi_id>/', views.visualizar_pdi, name='visualizar_pdi'),
     path('gerar-pdf/<int:pdi_id>/', views.gerar_pdf, name='gerar_pdf'),
-    # ... outras urls
 ]
