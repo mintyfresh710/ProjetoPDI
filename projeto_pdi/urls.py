cc92ebee1e6aa9b00e2a03692aac6ad0a04b93b6
Konkvon <joaopedroorem@gmail.com>
2025-06-26 22:05:21 -0300

    Delete projeto_pdi directory

--- projeto_pdi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("app_pdi/", include("app_pdi.urls")),
-    path("pdi_form/", include("pdi_form.urls")),
-    path("", include("app_pdi.urls")),
-]
